f257b0b474841bfbd2876f32cecf301fffc40885
Vikki <riverjoness@gmail.com>
2020-03-09 20:22:16 +0200

    division

---
+++ script2-3.js
@@ -0,0 +1,23 @@
+
+//Dividing of different types
+
+var  temp  = 4;
+console.log( temp /= 2  ); // 2
+
+console.log( "9" / "3"  ); // 3
+
+console.log( true / false  ); // 1/0 = Infifnity
+
+console.log( null / 2  ); // 0/2 = 0
+
+console.log( null / "2"  ); // 0/2 =0
+
+console.log( undefined / 2 ); // NaN
+
+console.log( undefined / "2" ); //NaN
+
+console.log( 5 %  3 ); // 2
+
+
+
+
