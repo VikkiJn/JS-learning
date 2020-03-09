e9a32e0e2b5b04c20f761017cde8996a97bc6a60
Vikki <riverjoness@gmail.com>
2020-03-09 18:44:07 +0200

    Update script2-1.js

--- script2-1.js
+++ script2-1.js
@@ -12,3 +12,16 @@ console.log( num3 += 2 ); //6
 let x  = 4;
 console.log( x = ++x ); //5
 
+
+//sum-string
+console.log( "" + 1 + 0 ); //"10"
+
+console.log( true + false ); // 1
+
+console.log( " -2 " + 4 ); // " -2 4 "
+
+console.log( null  + 1 ); // 1
+
+console.log( undefined  + 1 ); // NaN
+
+
