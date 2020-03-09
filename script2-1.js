463d7cf78754e493a3d3a9c9eedef1f9a2915436
Vikki <riverjoness@gmail.com>
2020-03-09 18:10:29 +0200

    summing
    1. Сложение различных типов

---
+++ script2-1.js
@@ -0,0 +1,14 @@
+// Сложение различных типов
+
+let num1  = 2;
+let num2 = 3;
+console.log( num1 + num2 ); //5
+
+//Совмещенный тип
+let num3  = 4;
+console.log( num3 += 2 ); //6
+
+//инкримент
+let x  = 4;
+console.log( x = ++x ); //5
+
