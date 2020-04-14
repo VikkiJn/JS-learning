2ee6e79299d2cb1abd405f90a6d7ba05cf5b46b8
Vikki <riverjoness@gmail.com>
2020-04-14 21:30:08 +0300

    date-and-time

---
+++ scrip10.js
@@ -0,0 +1,20 @@
+//Date and Time.
+//1. Получить текущую дату, вывести в консоль
+
+let currentDate = new Date();
+console.log(currentDate); 
+
+//2. Получить день, месяц и год текущей даты и по отдельности вывести в консоль
+
+
+let todayDate = new Date();
+let currentDay = todayDate.getDate();
+let currentMonth = todayDate.getMonth() + 1;
+let currentYear = todayDate.getFullYear();
+console.log('Day: ' + currentDay);
+console.log('Month: ' + currentMonth);
+console.log('Year: ' + currentYear); 
+
+//3. Решить задачи из ссылки на теорию 
+//На теорию задач из ссылки нет, только по практике.
+
